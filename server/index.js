c227a7256b0d541511e6dbfc985acf3a796f5974
Carla Sawana <strawkeiji@gmail.com>
2021-06-18 00:05:01 -0400

    client + grupos no banco

--- server/index.js
+++ server/index.js
@@ -5,10 +5,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const proxy = require('express-http-proxy');
-var oauthServer = require('express-oauth-server');
+// var oauthServer = require('express-oauth-server');
 const oauthServer2 = require('node-oauth2-server');
 const url = require('url');
 const authModel = require('./models/auth');
+const Connection = require('./controllers/Connection');
 
 const app = express();
 const port = process.env.PORT_SERVER || process.env.PORT || 8081;
@@ -55,12 +56,7 @@ app.oauth = new oauthServer2({
 });
 
 const authRouter = require('./router/authRouter');
-//
-// app.use((req,res, next)=>{
-//   console.log('Debug req1', req.body);
-//   console.log('Debug req2', req.url, req.method);
-//   next();
-// });
+
 
 app.use('/api', authRouter.authRouter(express.Router(), app));
 app.use(app.oauth.errorHandler())
@@ -70,35 +66,6 @@ app.get('/enter', app.oauth.authorise(), (req, res)=>{
 
   res.send('Parabéns, você está conectado');
 });
-
-
-// // Post token.
-// app.post('/oauth/token', app.oauth.token());
-//
-// // Get authorization.
-// app.get('/oauth/authorize', function(req, res) {
-//   // Redirect anonymous users to login page.
-//   if (!req.app.locals.user) {
-//     return res.redirect('/');
-//   }
-//
-//   return res.redirect('/');
-// });
-//
-// // Post authorization.
-// app.post('/oauth/authorize', function(req, res) {
-//   // Redirect anonymous users to login page.
-//   if (!req.app.locals.user) {
-//     return res.redirect(`/login?client_id=${req.query.client_id}&redirect_uri=${req.query.redirect_uri}`);
-//   }
-//
-//   return app.oauth.authorize();
-// });
-//
-// app.get('/getMessages/:id', messagesController.getMessages);
-// app.post('/api/signUp', userController.createUser);
-// app.post('/api/login', userController.loginUser);
-
 
 // Busca arquivos na pasta Dist
 if (process.env.NODE_ENV === 'production') {
@@ -125,43 +92,6 @@ const io = require("socket.io")(server, {
   },
 });
 
-//
-//
-// const emitMostRecentMessges = async () => {
-//   const result = await queryModel.getMessages();
-//   io.emit("chat message", result);
-// };
-
-const chats = {};
-
-
 io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("chatMessage", async (msg) => {
- //   await queryModel.createMessages(JSON.parse(msg));
-   // await emitMostRecentMessges();
-  });
-  socket.on("chatJoin", async (msg) => {
-    console.log('msg', msg);
-    const {id, name, userId} = msg;
-    if(!chats[id]) {
-      chats[id] = {
-        users: [],
-        name: name
-      };
-    }
-    chats[id].users.push({
-      socket: socket,
-      userId: userId,
-    })
-    console.log('msg', msg);
-    // socket.send("EAE!");
-    socket.emit("newMessage", "EAE");
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+  new Connection.Connection(socket);
 });
-
-
